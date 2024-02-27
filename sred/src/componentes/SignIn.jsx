46104483ec98e15c8c9c7179be0bfcd751c3a10b
RaulRiveraFP <120664361+RaulRiveraFP@users.noreply.github.com>
2024-02-27 19:52:58 +0000

    Conectar supabase

--- sred/src/componentes/SignIn.jsx
+++ sred/src/componentes/SignIn.jsx
@@ -1,14 +1,55 @@
+import React, { useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const supabaseUrl = 'https://sdyghacdmxuoytrtuntm.supabase.co'
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkeWdoYWNkbXh1b3l0cnR1bnRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkwNTkxNTksImV4cCI6MjAyNDYzNTE1OX0.dxlHJ9O4V2KZfC9yAGCLCHgKdVnLU41SWSXkzgohcvI'
+  const supabase = createClient(supabaseUrl, supabaseKey)
+
+  const handleSignIn = async () => {
+    try {
+      const { user, error } = await supabase.auth.signIn({
+        email,
+        password,
+      });
+
+      if (error) {
+        console.error('Error al iniciar sesión:', error.message);
+        alert('Usuario o contraseña incorrectos');
+      } else {
+        console.log('Usuario ha iniciado sesión correctamente:', user);
+      }
+    } catch (error) {
+      console.error('Error general:', error.message);
+    }
+  };
+
   return (
     <>
       <div className="container">
         <h1 className="mt-5 text-center">Inicia sesión</h1>
         <div className="m-5 mx-auto" style={{ maxWidth: '400px' }}>
-          <form action="" className="form border shadow-sm p-3">
+          <form className="form border shadow-sm p-3">
             <label htmlFor="email" className="form-label">Email:</label>
-            <input required id="email" type="email" className="form-control" />
+            <input
+              required
+              id="email"
+              type="email"
+              className="form-control"
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <label htmlFor="pass" className="form-label mt-3">Contraseña:</label>
-            <input required minLength="6" id="pass" type="password" className="form-control" />
+            <input
+              required
+              minLength="6"
+              id="pass"
+              type="password"
+              className="form-control"
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <div className="form-check mt-3">
               <input
                 className="form-check-input"
@@ -21,10 +62,20 @@ function SignIn() {
                 Recordar sesión
               </label>
             </div>
-            <a className="d-block text-end" href="#">¿Has olvidado tu contraseña?</a>
-            <a className="btn btn-primary w-100 mt-3" href="#">Iniciar sesión</a>
+            <a className="d-block text-end" href="#">
+              ¿Has olvidado tu contraseña?
+            </a>
+            <button
+              type="button"
+              className="btn btn-primary w-100 mt-3"
+              onClick={handleSignIn}
+            >
+              Iniciar sesión
+            </button>
           </form>
-          <a className="d-block mt-5 btn btn-secondary mx-auto" href="#">¿Eres nuevo? Regístrate</a>
+          <a className="d-block mt-5 btn btn-secondary mx-auto" href="#">
+            ¿Eres nuevo? Regístrate
+          </a>
         </div>
       </div>
     </>
@@ -32,4 +83,3 @@ function SignIn() {
 }
 
 export default SignIn;
-
