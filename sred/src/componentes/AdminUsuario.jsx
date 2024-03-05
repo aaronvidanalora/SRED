4e565f7b76ca1be03b70169109618d1f55f6f2b4
RaulRiveraFP <120664361+RaulRiveraFP@users.noreply.github.com>
2024-03-05 19:17:15 +0000

    Recintos, detalle recintos w/ supabase

--- sred/src/componentes/AdminUsuario.jsx
+++ sred/src/componentes/AdminUsuario.jsx
@@ -1,120 +1,136 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BiSearch, BiX, BiPencil, BiTrash, BiCaretDown } from 'react-icons/bi';
+import { createClient } from '@supabase/supabase-js';
 
 function AdminUsuario() {
-    return (
-        <>
-        <div className="container-fluid">
-            <h1 className="mt-5">Panel de administración</h1>
-            <div className="row mt-5">
-                <div className="col-12">
-                    <ul className="nav nav-tabs">
-                        <li className="nav-item w-50">
-                            <a className="nav-link active" aria-current="page" href="#">Usuarios</a>
-                        </li>
-                        <li className="nav-item w-50">
-                            <a className="nav-link" href="#">Recintos</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  const [usuarios, setUsuarios] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const supabaseUrl = 'https://your-supabase-url.supabase.co';
+  const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkeWdoYWNkbXh1b3l0cnR1bnRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkwNTkxNTksImV4cCI6MjAyNDYzNTE1OX0.dxlHJ9O4V2KZfC9yAGCLCHgKdVnLU41SWSXkzgohcvI';
+  const supabase = createClient(supabaseUrl, supabaseKey);
 
-            <div className="border border-top-0 p-3">
-                <div className="row">
-                    <div className="d-flex col-12 col-md-6 mb-3">
-                        <div className="input-group flex-nowrap">
-                            <span className="input-group-text" id="addon-wrapping">
-                                <BiSearch />
-                            </span>
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Buscador"
-                                aria-label="Username"
-                                aria-describedby="addon-wrapping"
-                            />
-                            <span className="input-group-text" id="addon-wrapping">
-                                <BiX />
-                            </span>
-                        </div>
-                    </div>
-                </div>
+  useEffect(() => {
+    const fetchUsuarios = async () => {
+      try {
+        const { data, error } = await supabase.from('usuarios').select();
+        if (error) {
+          console.error('Error fetching usuarios:', error);
+        } else {
+          setUsuarios(data || []);
+        }
+      } catch (error) {
+        console.error('Error fetching usuarios:', error);
+      }
+    };
 
-                <div className="col-12" style={{ overflowX: 'auto' }}>
-                    <table className="table table-striped table-bordered table-hover align-middle mt-3">
-                        <thead className="table-dark">
-                            <tr>
-                                <th></th>
-                                <th>
-                                    Nombre <span><BiCaretDown /></span>
-                                </th>
-                                <th>
-                                    Email <span><BiCaretDown /></span>
-                                </th>
-                                <th>
-                                    Rol <span><BiCaretDown /></span>
-                                </th>
-                                <th>
-                                    Fecha <span><BiCaretDown /></span>
-                                </th>
-                                <th>
-                                    DNI <span><BiCaretDown /></span>
-                                </th>
-                                <th></th>
-                                <th></th>
-                            </tr>
-                        </thead>
+    fetchUsuarios();
+  }, []);
 
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <div className="containerImagen">
-                                        <img width="50px" src="./src/assets/logo.jpg" alt="" />
-                                    </div>
-                                </td>
-                                <td>Usuario 1</td>
-                                <td>usuario@gmail.com</td>
-                                <td>Administrador</td>
-                                <td>31-12-2004</td>
-                                <td>53835091O</td>
-                                <td><i className="btn btn-outline-primary"><BiPencil /></i></td>
-                                <td><i className="btn btn-outline-danger"><BiTrash /></i></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div className="containerImagen">
-                                        <img width="50px" src="./src/assets/logo.jpg" alt="" />
-                                    </div>
-                                </td>
-                                <td>Usuario 1</td>
-                                <td>usuario@gmail.com</td>
-                                <td>Administrador</td>
-                                <td>31-12-2004</td>
-                                <td>53835091O</td>
-                                <td><i className="btn btn-outline-primary"><BiPencil /></i></td>
-                                <td><i className="btn btn-outline-danger"><BiTrash /></i></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div className="containerImagen">
-                                        <img width="50px" src="./src/assets/logo.jpg" alt="" />
-                                    </div>
-                                </td>
-                                <td>Usuario 1</td>
-                                <td>usuario@gmail.com</td>
-                                <td>Administrador</td>
-                                <td>31-12-2004</td>
-                                <td>53835091O</td>
-                                <td><i className="btn btn-outline-primary"><BiPencil /></i></td>
-                                <td><i className="btn btn-outline-danger"><BiTrash /></i></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+  const filteredUsuarios = usuarios.filter((usuario) =>
+    usuario.nombre.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <>
+      <div className="container-fluid">
+        <h1 className="mt-5">Panel de administración</h1>
+        <div className="row mt-5">
+          <div className="col-12">
+            <ul className="nav nav-tabs">
+              <li className="nav-item w-50">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Usuarios
+                </a>
+              </li>
+              <li className="nav-item w-50">
+                <a className="nav-link" href="#">
+                  Recintos
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        </>
-    )
+
+        <div className="border border-top-0 p-3">
+          <div className="row">
+            <div className="d-flex col-12 col-md-6 mb-3">
+              <div className="input-group flex-nowrap">
+                <span className="input-group-text" id="addon-wrapping">
+                  <BiSearch />
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Buscador"
+                  aria-label="Buscador"
+                  aria-describedby="addon-wrapping"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <span className="input-group-text" id="addon-wrapping">
+                  <BiX />
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12" style={{ overflowX: 'auto' }}>
+            <table className="table table-striped table-bordered table-hover align-middle mt-3">
+              <thead className="table-dark">
+                <tr>
+                  <th></th>
+                  <th>
+                    Nombre <span><BiCaretDown /></span>
+                  </th>
+                  <th>
+                    Email <span><BiCaretDown /></span>
+                  </th>
+                  <th>
+                    Rol <span><BiCaretDown /></span>
+                  </th>
+                  <th>
+                    Fecha <span><BiCaretDown /></span>
+                  </th>
+                  <th>
+                    DNI <span><BiCaretDown /></span>
+                  </th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {filteredUsuarios.map((usuario) => (
+                  <tr key={usuario.id}>
+                    <td>
+                      <div className="containerImagen">
+                        <img width="50px" src={usuario.imagen} alt="" />
+                      </div>
+                    </td>
+                    <td>{usuario.nombre}</td>
+                    <td>{usuario.email}</td>
+                    <td>{usuario.rol}</td>
+                    <td>{usuario.fecha}</td>
+                    <td>{usuario.dni}</td>
+                    <td>
+                      <i className="btn btn-outline-primary">
+                        <BiPencil />
+                      </i>
+                    </td>
+                    <td>
+                      <i className="btn btn-outline-danger">
+                        <BiTrash />
+                      </i>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default AdminUsuario
+export default AdminUsuario;
--- sred/src/componentes/Header.jsx
+++ sred/src/componentes/Header.jsx
@@ -10,6 +10,7 @@ import Reservar from './Reservar';
 import Recintos from './Recintos';
 import AdminUsuario from './AdminUsuario';
 import MisReservas from './MisReservas';
+import RegistroRecintos from './RegistraRecintos';
 
 function Header() {
   return (
@@ -61,11 +62,12 @@ function Header() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/detallerecinto" element={<DetalleRecinto />} />
+        <Route path="/detalle-recinto/:id" element={<DetalleRecinto/>} />
         <Route path="/adminrecinto" element={<AdminRecinto />} />
         <Route path="/editarecinto" element={<EditaRecinto />} />
         <Route path="/recintos" element={<Recintos />} />
         <Route path="/reservarecinto" element={<Reservar />} />
+        <Route path="/registrarecinto" element={<RegistroRecintos />} />
         <Route path="/adminusuarios" element={<AdminUsuario />} />
         <Route path="/reservas" element={<MisReservas />} />
       </Routes>
