2e3bbd619860fde000b21518f33ec4136fd0fb15
adriansanchezlarrondo <123000464+adriansanchezlarrondo@users.noreply.github.com>
2024-03-05 19:42:17 +0000

    admin recintos

--- sred/src/componentes/AdminRecinto.jsx
+++ sred/src/componentes/AdminRecinto.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { BiSearch, BiX, BiPencil, BiTrash, BiCaretDown } from 'react-icons/bi';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { createClient } from '@supabase/supabase-js';
 
 function AdminRecinto() {
