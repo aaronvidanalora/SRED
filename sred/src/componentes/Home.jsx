40e1dda9edef19ae7cea851de5e373f638a72d14
aaronvidanalora <120584227+aaronvidanalora@users.noreply.github.com>
2024-05-14 14:49:35 +0000

    pequeño error home padding negro abajo

--- sred/src/componentes/Home.jsx
+++ sred/src/componentes/Home.jsx
@@ -4,7 +4,7 @@ import '../App.css';
 function Home() {
   return (
     <>
-      <div className="container mx-auto pt-lg-5">
+      <div className="container mx-auto pt-lg-5 pb-3">
         <h1 className="mt-5 text-center fw-bold" style={{fontSize: '60px'}}>Sistema Reservas Espacios Deportivos</h1>
         <div className="my-5 d-flex justify-content-center w-100">
             <img src="/logo-blanco.png" alt="fpllefia" className=""/>
