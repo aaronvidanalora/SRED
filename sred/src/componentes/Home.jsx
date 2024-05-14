a5eab5bbc886b68caa619e065e0de9e54e022395
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-05-14 17:08:16 +0200

    .

--- sred/src/componentes/Home.jsx
+++ sred/src/componentes/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <div className="container mx-auto pt-lg-5 pb-3">
         <h1 className="mt-5 text-center fw-bold" style={{fontSize: '60px'}}>Sistema Reservas Espacios Deportivos</h1>
         <div className="my-5 d-flex justify-content-center w-100">
-            <img src="/logo-blanco.png" alt="fpllefia" className=""/>
+            <img src="/logo-blanco.png" alt="logo-sred"/>
         </div>
       </div>
     </>
